32d510f4ff12be8d60be42d69c6ce8a4b5590d5d
Sophus David <95245426@qq.com>
2023-04-19 21:24:05 +0800

    add get_freq_mask

---
+++ freenerf/util.py
@@ -0,0 +1,15 @@
+import torch 
+
+
+def get_freq_mask(pos_enc_length, current_iter, total_reg_iter):
+    if current_iter < total_reg_iter:
+        freq_mask=torch.zeros(pos_enc_length)
+        ptr = pos_enc_length / 3 * current_iter / total_reg_iter + 1
+        ptr = ptr if ptr < pos_enc_length / 3 else pos_enc_length / 3
+        int_ptr = int(ptr)
+        freq_mask[: int_ptr * 3] = 1.0  # assign the integer part
+        freq_mask[int_ptr * 3 : int_ptr * 3 + 3] = (ptr - int_ptr)  # assign the fractional part
+        return torch.clip(freq_mask,1e-8, 1-1e-8) # for math stabiltiy
+    else:
+        return torch.ones(pos_enc_length)
+    
